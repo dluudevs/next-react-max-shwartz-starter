b1009ac5b0002f1d6eea1c61247ac8b090aca11f
Derek <dluu@trewknowledge.com>
2021-04-11 18:01:08 -0400

    created events-search ui

--- components/ui/button.js
+++ components/ui/button.js
@@ -3,11 +3,19 @@ import Link from "next/link";
 import classes from "./buttons.module.css";
 
 function Button(props) {
+  if (props.link) {
+    return (
+      <Link href={props.link}>
+        {/* by default Link will create an anchor tag and apply attributes to the a tag, if we want to apply classNames we have to add an a tag manually */}
+        <a className={classes.btn}>{props.children}</a>
+      </Link>
+    );
+  }
+
   return (
-    <Link href={props.link}>
-      {/* by default Link will create an anchor tag and apply attributes to the a tag, if we want to apply classNames we have to add an a tag manually */}
-      <a className={classes.btn}>{props.children}</a>
-    </Link>
+    <button className={classes.btn} onClick={props.onClick}>
+      {props.children}
+    </button>
   );
 }
 
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,14 +1,16 @@
-import { getAllEvents } from '../../dummy-data'
-import EventList from '../../components/events/event-list'
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 
 const AllEventsPage = () => {
-  const events = getAllEvents()
-  
+  const events = getAllEvents();
+
   return (
-    <div>
+    <>
+      <EventsSearch />
       <EventList items={events} />
-    </div>
-  )
-}
+    </>
+  );
+};
 
 export default AllEventsPage;
