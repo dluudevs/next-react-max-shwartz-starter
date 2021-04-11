3fd32e6623b07879cf91edcaa8e91d57e0268dd8
Derek <dluu@trewknowledge.com>
2021-04-11 18:20:41 -0400

    add functionality to events search to navigate to filter events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,22 @@
+import { useRouter } from "next/router";
+
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 
 const AllEventsPage = () => {
   const events = getAllEvents();
+  const router = useRouter();
+
+  // Programmatically navigate - navigate to a different page after an action is done
+  // EventsPage handles it as we want pages to handle navigations, not components (EventsSearch)
+  const findEventsHandler = (year, month) => {
+    router.push(`/events/${year}/${month}`)
+  };
 
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </>
   );
