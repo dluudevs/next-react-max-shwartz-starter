2d16ba657674ef212662d18f755afc80aa390356
Derek <dluu@trewknowledge.com>
2021-04-11 17:41:57 -0400

    created header for all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
-import Head from 'next/head';
+import Head from "next/head";
+
+import Layout from "../components/layout/layout";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-      </Head>
+    <Layout>
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 }
 
