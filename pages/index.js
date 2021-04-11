59cb024e87522473100d5fbd507e87f6cdb1fb5b
Derek <dluu@trewknowledge.com>
2021-04-11 17:41:25 -0400

    render EventList on home page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
+import { getFeaturedEvents } from '../dummy-data';
+import EventList from '../components/events/event-list'
+
 const HomePage = () => {
+  const featuredEvents = getFeaturedEvents()
+  
   return (
-    <h1>This is the home page</h1>
+    <div>
+      <EventList items={featuredEvents}/>
+    </div>
   )
 }
 
