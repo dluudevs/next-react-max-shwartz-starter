4bbd363f013ff1eda08941b08b010364b7168ffa
Derek <dluu@trewknowledge.com>
2021-04-29 22:25:51 -0400

    create utils for fetching data from firebase

--- utils/api-util.js
+++ utils/api-util.js
@@ -3,7 +3,7 @@ export async function getAllEvents() {
     "https://nextjs-course-max-schwartz-default-rtdb.firebaseio.com/events.json"
   );
   const data = await response.json();
-  
+
   const events = [];
   for (const key in data) {
     events.push({
@@ -16,6 +16,25 @@ export async function getAllEvents() {
 }
 
 export async function getFeaturedEvents() {
-  const events = await getAllEvents() 
+  const events = await getAllEvents();
   return events.filter((event) => event.isFeatured);
+}
+
+export async function getEventById(id) {
+  const events = await getAllEvents();
+  return events.find((event) => event.id === id);
+}
+
+export async function getFilteredEvents(dateFilter) {
+  const { year, month } = dateFilter;
+
+  const events = await getAllEvents();
+  const filteredEvents = events.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+    );
+  });
+
+  return filteredEvents;
 }
